17d3716ce87f15016ef7861dd359f728acca32e3
ShehrozEdu <battshehrozedu@gmail.com>
2022-12-07 12:19:09 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -15,49 +15,49 @@ app.use("/api", Router);
 
 //SOCKET IO TEST
 
-const io = require("socket.io")(9000, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
-});
+// const io = require("socket.io")(9000, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//   },
+// });
 
-let users = [];
+// let users = [];
 
-const addUser = (userData, socketId) => {
-  !users.some((user) => user.sub === userData.sub) &&
-    users.push({ ...userData, socketId });
-};
+// const addUser = (userData, socketId) => {
+//   !users.some((user) => user.sub === userData.sub) &&
+//     users.push({ ...userData, socketId });
+// };
 
-const removeUser = (socketId) => {
-  users = users?.filter((user) => user.socketId !== socketId);
-};
+// const removeUser = (socketId) => {
+//   users = users?.filter((user) => user.socketId !== socketId);
+// };
 
-const getUser = (userId) => {
-  return users.find((user) => user.sub === userId);
-};
+// const getUser = (userId) => {
+//   return users.find((user) => user.sub === userId);
+// };
 
-io.on("connection", (socket) => {
-  console.log("user connected");
+// io.on("connection", (socket) => {
+//   console.log("user connected");
 
-  //connect
-  socket.on("addUser", (userData) => {
-    addUser(userData, socket.id);
-    io.emit("getUsers", users);
-  });
+//   //connect
+//   socket.on("addUser", (userData) => {
+//     addUser(userData, socket.id);
+//     io.emit("getUsers", users);
+//   });
 
-  //send message
-  socket.on("sendMessage", (data) => {
-    const user = getUser(data.receiverId);
-    io.to(user.socketId).emit("getMessage", data);
-  });
+//   //send message
+//   socket.on("sendMessage", (data) => {
+//     const user = getUser(data.receiverId);
+//     io.to(user.socketId).emit("getMessage", data);
+//   });
 
-  //disconnect
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-    removeUser(socket.id);
-    io.emit("getUsers", users);
-  });
-});
+//   //disconnect
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//     removeUser(socket.id);
+//     io.emit("getUsers", users);
+//   });
+// });
 
 //Server
 mongoose
