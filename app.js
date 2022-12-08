1a52580c73d2cb075f5eb92f8970c6d5294c962d
ShehrozEdu <battshehrozedu@gmail.com>
2022-12-08 18:46:39 +0530

    test

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const server = app.listen(process.env.PORT, () => {
 const io = require("socket.io")(server, {
   cors: {
     origin: "https://whatsapp-clone-1.netlify.app",
+    transports: ["websocket"],
   },
 });
 
