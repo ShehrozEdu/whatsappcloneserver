6f510e91b27564998b5711f2e375c712c3179e08
ShehrozEdu <battshehrozedu@gmail.com>
2022-12-09 18:16:29 +0530

    Updated img error

--- app/controllers/FileController.js
+++ app/controllers/FileController.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const grid = require("gridfs-stream");
 
-const url = "http://localhost:4000/api";
+const url = "https://whatsapp-clone-1.netlify.app//api";
 
 let gfs;
 let gridFsBucket;
